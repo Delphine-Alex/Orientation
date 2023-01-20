d1c8db8795eb06b89e1cc73245b740777f0ec85d
Delphine <delphine.pva@gmail.com>
2023-01-20 11:49:33 +0100

    Generate QR Code works

--- Orientation/src/screens/Setting/index.js
+++ Orientation/src/screens/Setting/index.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-const Setting = () => {
+import QRCode from 'react-native-qrcode-svg';
+
+import styled from 'styled-components';
+
+const App = () => {
+    const [input, setInput] = useState('');
+    const [qrvalue, setQrvalue] = useState('');
+
     return (
         <View>
-            <Text>Hello world!</Text>
+            {qrvalue ? (
+                <>
+                    <QRCode
+                        value={qrvalue}
+                        size={250}
+                        color="black"
+                        backgroundColor="white"
+                    />
+                    <Text>Please insert any value to generate QR code</Text>
+
+                    <TextInput
+                        onChangeText={(input) => setInput(input)}
+                        placeholder="Enter Any Value here"
+                        value={input}
+                    />
+                    <TouchableOpacity onPress={() => setQrvalue(input)}>
+                        <Text>Generate QR Code</Text>
+                    </TouchableOpacity>
+                </>
+            ) : (
+                <>
+                    <Text>Please insert any value to generate QR code</Text>
+
+                    <TextInput
+                        onChangeText={(input) => setInput(input)}
+                        placeholder="Enter Any Value here"
+                        value={input}
+                    />
+                    <TouchableOpacity onPress={() => setQrvalue(input)}>
+                        <Text>Generate QR Code</Text>
+                    </TouchableOpacity>
+                </>
+            )}
         </View>
     );
-}
+};
 
-export default Setting;
+export default App;
