d5975ee2881818b5536a057bac5dd13d9c0e363b
AdrienDucourthial <adu@evertrust.fr>
2023-01-21 14:24:26 +0100

    i added the component to read code QR

--- Orientation/src/screens/Run/index.js
+++ Orientation/src/screens/Run/index.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { Button, Dimensions, StyleSheet, View } from "react-native";
+import { Button, View } from "react-native";
 import MapView from 'react-native-maps';
 import styled from "styled-components";
+import ScanQRCode from "../../component/ScanQRCode";
+
+
 
 const Run = ({navigation}) => {
   return (
     <View>
       <StyledMap />
+      <ScanQRCode />
       <Button onPress={() => navigation.navigate('Home')} title="Home" /> 
+      <Button onPress={() => navigation.navigate('test')} title="Test" /> 
     </View>
   );
-
-
 }
 
 const StyledMap = styled(MapView) `
---
+++ Orientation/src/screens/SetupRace/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-native";
+
+const SetupRace = () => {
+  return (
+    <View>
+      <Button title="submit" />
+    </View>
+  )
+}
+
+export default SetupRace;
