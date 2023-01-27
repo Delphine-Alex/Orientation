8e137d00a9ce0bf81ca4dad2daaf6567dd130368
Delphine <delphine.pva@gmail.com>
2023-01-27 14:22:44 +0100

    save works, all works

--- Orientation/src/screens/PreRun/index.js
+++ Orientation/src/screens/PreRun/index.js
@@ -1,3 +1,72 @@
+// import React, { useEffect, useState } from "react";
+// import axios from 'axios';
+
+// import { View, Text } from 'react-native';
+
+// import { useNavigation } from '@react-navigation/native';
+
+// import styled from "styled-components";
+
+// const PreRun = (route) => {
+//     const [race, setRace] = useState({});
+//     // const [allTag, setAllTag] = useState({});
+//     // const [selectedTag, setSelectedTag] = useState({});
+
+//     const navigation = useNavigation();
+
+//     useEffect(() => {
+//         const getRace = async () => {
+//             try {
+//                 const result = await axios.get(`https://archilogllele.azurewebsites.net/api/Race/${route.params.id}`)
+//                 setRace(result.data);
+//                 console.log(result.data);
+//                 // getTag();
+//             } catch (error) {
+//                 console.log(error)
+//             }
+//         }
+//         // const getTag = async () => {
+//         //     try {
+//         //         const result2 = await axios.get(`https://archilogllele.azurewebsites.net/api/Tag`)
+//         //         setAllTag(result2.data)
+//         //         console.log(result2.data)
+//         //         const temp = []
+//         //         for (let index = 0; index < allTag.length; index++) {
+//         //             if (allTag[index].idRace === race.id) {
+//         //                 temp.push(allTag[index])
+//         //             }
+//         //         }
+//         //         setSelectedTag(temp)
+//         //         console.log('selectTags', selectedTag)
+//         //     } catch (error) {
+//         //         console.log(error)
+//         //     }
+//         // }
+//         getRace()
+//     }, []);
+
+//     return (
+//         <Container>
+//             <BackButton onPress={() => navigation.navigate('Preparation')}>Back</BackButton>
+//             <Text>{race.name}</Text>
+//         </Container>
+//     );
+// }
+
+// const Container = styled.View`
+//     background-color: white;
+//     padding: 0 6% 2.5% 6%;
+//     height: 100%;
+// `
+// const BackButton = styled.Text`
+//     margin-bottom: 25px;
+//     font-weight: bold;
+//     font-size: 14px;
+//     color: black;
+// `
+
+// export default PreRun;
+
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
@@ -12,9 +81,8 @@ const PreRun = (race) => {
 
     return (
         <Container>
-            <BackButton onPress={() => navigation.navigate('Run')}>Back</BackButton>
+            <BackButton onPress={() => navigation.navigate('Preparation')}>Back</BackButton>
             <Text>{race && race.route.params.name}</Text>
-
         </Container>
     );
 }
